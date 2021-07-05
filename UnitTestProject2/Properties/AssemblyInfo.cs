ef4e7ee4dd46a6af85cb83c99d698a258e147b65
Lolipop <pacha_vlasov_95@mail.ru>
2021-07-05 12:32:13 +0300

    U

---
+++ UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6ce69f6d-b200-485b-9122-d0caca2bf9c7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Critical;
+using Microsoft.Win32;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [STAThread]
+        static string Dialog()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.FileName = "Ввод.csv";
+            dlg.DefaultExt = ".csv";
+            dlg.Filter = "CSV documents (.csv)|*.csv";
+            dlg.ShowDialog();
+            return dlg.FileName;
+        }
+        Critic Cr = new Critic();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var Test = Cr.Input(Dialog());
+            Assert.AreEqual(Cr.MaxElem(Test), 8);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var Test = Cr.Input(Dialog());
+            Assert.AreEqual(Cr.MinElem(Test), 2);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var Test = Cr.Input(Dialog());
+            Assert.AreEqual(Cr.LenFunc(Test), 43);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(Cr.s, "");
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.IsInstanceOfType(Cr.s, typeof(string));
+        }
+    }
+}
